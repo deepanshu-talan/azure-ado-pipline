1a0475443d817002b6ab91586e810788323bc145
Deepanshu Talan <deepanshutalan71@gmail.com>
2026-08-13 18:31:43 +0530

    fixed alias errors

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "azurerm_storage_account" "stg" {
 
 resource "azurerm_storage_container" "cntr" {
   name                  = var.container_name
-  storage_account_id    = azurerm_storage_account.cntr.id
+  storage_account_id    = azurerm_storage_account.stg.id
   container_access_type = "private"
 }
 
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "resource_group_name" {
   description = "Name of the created resource group."
-  value       = azurerm_resource_group.stg.name
+  value       = azurerm_resource_group.rg.name
 }
 
 output "resource_group_id" {
   description = "ID of the created resource group."
-  value       = azurerm_resource_group.stg.id
+  value       = azurerm_resource_group.rg.id
 }
 
 output "storage_account_name" {

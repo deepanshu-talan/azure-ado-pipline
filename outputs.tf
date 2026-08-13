993ce0b9d5b11e2f6779d55376576e0af11bdd11
Deepanshu Talan <deepanshutalan71@gmail.com>
2026-08-13 18:28:00 +0530

    fixed lable errors

--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,34 @@
 output "resource_group_name" {
   description = "Name of the created resource group."
-  value       = azurerm_resource_group.main.name
+  value       = azurerm_resource_group.stg.name
 }
 
 output "resource_group_id" {
   description = "ID of the created resource group."
-  value       = azurerm_resource_group.main.id
+  value       = azurerm_resource_group.stg.id
 }
 
 output "storage_account_name" {
   description = "Name of the created storage account."
-  value       = azurerm_storage_account.main.name
+  value       = azurerm_storage_account.stg.name
 }
 
 output "storage_account_id" {
   description = "ID of the created storage account."
-  value       = azurerm_storage_account.main.id
+  value       = azurerm_storage_account.stg.id
 }
 
 output "storage_container_name" {
   description = "Name of the created blob container."
-  value       = azurerm_storage_container.main.name
+  value       = azurerm_storage_container.cntr.name
 }
 
 output "log_analytics_workspace_name" {
   description = "Name of the Log Analytics workspace."
-  value       = azurerm_log_analytics_workspace.main.name
+  value       = azurerm_log_analytics_workspace.law.name
 }
 
 output "log_analytics_workspace_id" {
   description = "ID of the Log Analytics workspace."
-  value       = azurerm_log_analytics_workspace.main.id
+  value       = azurerm_log_analytics_workspace.law.id
 }
